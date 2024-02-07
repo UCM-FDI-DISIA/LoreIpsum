1b1037d44fb9f64d68bd3ca1a51b6d04f0d1739b
Paulalemany <palemany@ucm.es>
2024-02-07 09:47:00 +0100

    página web subida

---
+++ web/script.js
@@ -0,0 +1,8 @@
+function devuelveTextoDeAlerta() {
+  return "uooooo! Vaya alerta";
+}
+
+function desaparece(nombre) {
+	var button = document.getElementById(nombre);
+  button.style.visibility='hidden';
+}
