93640753e034fed610a341460e2038a7f693d4d5
itsJ1mbo <jaimev02@ucm.es>
2024-02-12 16:57:14 +0100

    operator= transform

--- Hollenhaus/Hollenhaus proyect VS/src/Transform.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/Transform.cpp
@@ -5,4 +5,15 @@ Transform::operator+(const Transform& t) {
 	pos = pos + t.pos;
 	scale = scale + t.scale;
 	angle += t.angle;
+
+	return *this;
+}
+
+Transform&
+Transform::operator=(const Transform& t) {
+	pos = t.pos;
+	scale = t.scale;
+	angle = t.angle;
+
+	return *this;
 }
