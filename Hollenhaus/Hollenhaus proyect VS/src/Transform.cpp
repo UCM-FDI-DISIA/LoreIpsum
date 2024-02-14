30d36785de4013888231b62a951fc53715b4dbcb
Jimbo <javicentear@gmail.com>
2024-02-14 12:31:48 +0100

    cosas del transform

--- Hollenhaus/Hollenhaus proyect VS/src/Transform.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/Transform.cpp
@@ -1,19 +1,48 @@
 #include "Transform.h"
 
+void
+Transform::addParent(Transform* p) {
+	if (!isChild_) {
+		parent_ = p;
+		pos_ = pos_ - parent_->pos_; // Pos realtiva al padre
+		isChild_ = true;
+	}
+}
+
+void
+Transform::update() {
+	if (isChild_);
+		// Movimiento con padre
+	else;
+		// Sin padre
+}
+
 Transform& 
 Transform::operator+(const Transform& t) {
-	pos = pos + t.pos;
-	scale = scale + t.scale;
-	angle += t.angle;
+	pos_ = pos_ + t.pos_;
+	scale_ = scale_ + t.scale_;
+	angle_ += t.angle_;
+	vel_ = vel_ + t.vel_;
+
+	return *this;
+}
+
+Transform&
+Transform::operator-(const Transform& t) {
+	pos_ = pos_ - t.pos_;
+	scale_ = scale_ - t.scale_;
+	angle_ -= t.angle_;
+	vel_ = vel_ - t.vel_;
 
 	return *this;
 }
 
 Transform&
 Transform::operator=(const Transform& t) {
-	pos = t.pos;
-	scale = t.scale;
-	angle = t.angle;
+	pos_ = t.pos_;
+	scale_ = t.scale_;
+	angle_ = t.angle_;
+	vel_ = t.vel_;
 
 	return *this;
 }
--- Hollenhaus/Hollenhaus proyect VS/src/Transform.h
+++ Hollenhaus/Hollenhaus proyect VS/src/Transform.h
@@ -5,18 +5,36 @@ class Transform :
     public ComponentUpdate
 {
 public:
-    Transform() : parent(), angle() {
-        if (parent != nullptr);
+    Transform() : parent_(), angle_(), isChild_(false) {};
+    ~Transform() {
+        delete parent_;
+        parent_ = nullptr;
     };
-    ~Transform() {};
+
+    void update() override;
+
+    // Añadir un padre
+    void addParent(Transform* p);
+    // Getters
+    Vector2D getPos() { 
+        if (isChild_)
+            return pos_ + parent_->pos_;
+        else
+            return pos_;
+    };
+    Vector2D getScale() { return scale_; };
+    float getAngle() { return angle_; };
 
     Transform& operator+(const Transform& t);
+    Transform& operator-(const Transform& t);
     Transform& operator=(const Transform& t);
 private:
-    Transform* parent;
+    Transform* parent_;
+    bool isChild_;
 
-    Vector2D pos,
-             scale;
-    float angle;
+    Vector2D pos_,
+             vel_,
+             scale_;
+    float angle_;
 };
 
