777bc995d03e73f4b5f5cb043968f575d1034610
Inés <ineprimo@ucm.es>
2024-02-15 12:21:46 +0100

    colorines base

--- Hollenhaus/Hollenhaus proyect VS/src/game/Board.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/Board.cpp
@@ -1,14 +1,37 @@
 #include "Board.h"
+#include <iostream>
+#include <stdLib.h>
 
 Board::Board()
 {
 	IniciaTablero();
 
+	// prepara el color para consola con Windows.h
+	//HANDLE console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+
 }
 
 void Board::PaintBoard()
 {
+	// para colorear poner 
+	// system("Color E4");
+	// siendo E el color de fondo y 4 el del texto
+	// despues poner de vuelta el 
+	// system("Color 07") para blanco y negro
 
+	for (int i = 0; i < mazo.size(); i++) {
+
+		// pinta la carta
+		std::cout << GetCard(nullptr);
+
+		// cada 4 hace un salto de linea
+		if (i % 4 == 0) {
+			std::cout << "\n";
+		}
+
+	}
 
 }
 
@@ -28,10 +51,12 @@ std::string Board::getEffect(Card* card)
 
 void Board::IniciaTablero()
 {
+
+	/*
 	std::string skill = "->+2";
 	std::string sprite = "yippie";
 	Card carta = Card(1, 2, sprite, skill);
 
-	mazo.push_back(&carta);
+	mazo.push_back(&carta);*/
 
 }
