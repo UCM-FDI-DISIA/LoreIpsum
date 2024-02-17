ff4011e5fe5696ab8cf612e0afe189e00fa3ba5f
AndresGArcia <andrga37@ucm.es>
2024-02-17 17:53:52 +0100

    creo que ya está el bojs colaider

--- Hollenhaus/Hollenhaus proyect VS/src/BoxCollider.h
+++ Hollenhaus/Hollenhaus proyect VS/src/BoxCollider.h
@@ -8,9 +8,30 @@
 class BoxCollider :public ComponentUpdate
 {
 public:
+	/// <summary>
+	/// constructora vacía, por defecto posOffset = (0,0) / size = (1,1)
+	/// </summary>
 	BoxCollider();
+	/// <param name="posOffset"> Por defecto esta a 0</param>
+	/// <param name="size"> Por defecto esta a 1</param>
+	BoxCollider(Vector2D posOffset, Vector2D size);
 
 	void initComponent() override;
+
+	/// <summary>
+	/// Para cambiar el offset de la posicion del collider
+	/// </summary>
+	/// <param name="newPosOffset"></param>
+	void setPosOffset(Vector2D newPosOffset);
+	Vector2D getPosOffset() { return posOffset_; }
+
+	/// <summary>
+	/// Para cambiar el tamaño del collider
+	/// </summary>
+	/// <param name="newPosOffset"></param>
+	void setSize(Vector2D newSize);
+	Vector2D getSize() { return size_; }
+	SDL_Rect* getRect() { return &collider_; }
 
 	void update() override;
 
@@ -18,6 +39,7 @@ private:
 	Transform* transform_;
 	SpriteRenderer* spriteRenderer_;
 	SDL_Rect collider_;
-	Vector2D offset_;
+	Vector2D posOffset_;
+	Vector2D size_;
 };
 
