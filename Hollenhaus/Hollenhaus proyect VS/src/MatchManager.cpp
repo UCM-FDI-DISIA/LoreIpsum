5fb921901521a74b6c343a7b1477c11a3dfc31df
nievesag <nievalon@ucm.es>
2024-02-18 20:30:38 +0100

    jopelines tio

--- Hollenhaus/Hollenhaus proyect VS/src/MatchManager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/MatchManager.cpp
@@ -1,1 +1,8 @@
 #include "MatchManager.h"
+
+int MatchManager::whoWon()
+{
+	if (playerScore > opponentScore) { return matchResult = PLAYERWON; } // gana player
+	else if (opponentScore > playerScore) { return matchResult = OPPONENTWON; } // gana opponent
+	else if (playerScore = opponentScore) { return matchResult = TIE; } // empate
+}
--- Hollenhaus/Hollenhaus proyect VS/src/MatchManager.h
+++ Hollenhaus/Hollenhaus proyect VS/src/MatchManager.h
@@ -3,14 +3,18 @@
 class MatchManager
 {
 private:
-
 	// score player 1 & 2
 	int playerScore = 0;
 	int opponentScore = 0;
 
 public:
+	int matchResult;
 
-	// bool matchWon
+	enum matchResults {
+		PLAYERWON = 0,
+		OPPONENTWON = 1,
+		TIE = 2
+	};
 
 	// aumenta la puntuacion del jugador
 	void increasePlayerScore(int add) {
@@ -21,5 +25,7 @@ public:
 	void increaseOppentScore(int add) {
 		playerScore += add;
 	}
+
+	int whoWon();
 };
 
