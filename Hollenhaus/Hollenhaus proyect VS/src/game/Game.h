47714b09d9102a776a34e7258e6de498090aa9e7
Paulalemany <palemany@ucm.es>
2024-02-19 15:37:39 +0100

    compila :)

--- Hollenhaus/Hollenhaus proyect VS/src/game/Game.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/Game.h
@@ -6,6 +6,7 @@
 
 class GameStateMachine;
 
+//Probablemente haya que hacer un enum de 
 class Game {
 public:
 	Game();
@@ -15,12 +16,37 @@ public:
 	
 private:
 
-	GameStateMachine* gameStateMachine;
+#pragma region parámetros
 
 	Uint32 frameTime;
 	Uint32 startTime;
 
 	const Uint8 FRAME_RATE = 20;
+#pragma endregion
+
+#pragma region Maquina de estados
+
+	GameStateMachine* gameStateMachine;
+
+	//Enum de estados del juego
+	enum game_states {
+		MAINMENU = 0,
+		CITY,
+		OFFICE,
+		SHOP,
+		PLAY
+	};
+
+	//Estado actual del juego
+	int _state;
+#pragma endregion
+
+	//Texturas
+	//TODO
+
+	//Haría falta un booleano de salida
+
+
 
 	void Render() const;
 	void Update();
