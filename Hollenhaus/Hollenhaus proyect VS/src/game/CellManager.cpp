4e0dc4ff63ef94377e316f0a32e08e63f0de629d
Lparres2000 <Lparres2000@gmail.com>
2024-02-21 18:03:52 +0100

    Board terminado (falta testear)

--- Hollenhaus/Hollenhaus proyect VS/src/game/CellManager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/CellManager.cpp
@@ -1,5 +1,23 @@
 #include "CellManager.h"
+#include "Entity.h"
+CellManager::CellManager() :
+	_card(nullptr),
+	posOnBoard(-1, -1)
+{}
 
-CellManager::CellManager()
+
+bool CellManager::SetCard(ecs::entity_t card)
 {
+	return true;
+}
+
+void CellManager::setPosOnBoard(int posX, int posY)
+{
+	posOnBoard.setX(posX);
+	posOnBoard.setY(posY);
+}
+
+void CellManager::setPosOnBoard(Vector2D pos)
+{
+	posOnBoard = pos;
 }
