ab694a804cc0b00b67006fe4215a5093d6eeb0ac
samuel <samuga05@ucm.es>
2024-02-22 13:11:12 +0100

    ajustes en el deck para que no pete

--- Hollenhaus/Hollenhaus proyect VS/src/game/DeckComponent.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/DeckComponent.cpp
@@ -4,16 +4,16 @@ void
 DeckComponent::shuffle() {
 	// Puede que miki llore con esto no lo se no conozco el coste de estos algoritmos confio en c++
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));
+	//std::shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));
 }
 
 void 
 DeckComponent::removeCard(Card* c) {
-	deck.remove(*c);
+	deck.remove(c);
 }
 
 void 
-DeckComponent::addCardsOnBottom(std::list<Card> h) {
+DeckComponent::addCardsOnBottom(std::list<Card*> h) {
 	while (!h.empty()) {
 		deck.push_front(h.front());
 		h.pop_front();
@@ -24,7 +24,7 @@ DeckComponent::addCardsOnBottom(std::list<Card> h) {
 
 Card*
 DeckComponent::drawCard() {
-	Card* c = &*deck.end();
+	Card* c = *deck.end();
 	removeCard(c);
 
 	return c;
