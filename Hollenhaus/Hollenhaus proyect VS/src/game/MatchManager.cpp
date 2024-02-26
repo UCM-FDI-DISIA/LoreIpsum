478738ad71a9af304e281633d026f2d4d00c4940
Luis <lparres@ucm.es>
2024-02-26 15:11:15 +0100

    Match Manager

---
+++ Hollenhaus/Hollenhaus proyect VS/src/game/MatchManager.cpp
@@ -0,0 +1,20 @@
+#include "MatchManager.h"
+
+MatchManager::MatchManager() :
+	defaultActionPoints(4),
+	actualActionPoints(4)
+{
+
+}
+
+MatchManager::~MatchManager()
+{
+}
+
+void MatchManager::initComponent()
+{
+}
+
+void MatchManager::update()
+{
+}
