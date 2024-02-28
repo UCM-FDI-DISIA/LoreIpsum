5217cd0102f8264f7b46e1231622387485a79332
ariadnarc <ariadr02@ucm.es>
2024-02-28 17:28:00 +0100

    posibles problemas de mergeo pero

--- Hollenhaus/Hollenhaus proyect VS/src/game/Transform.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/Transform.h
@@ -45,6 +45,10 @@ public:
     // Rotacion relativa al padre
     //
     float& getRelativeAngle() { return relativeAngle_; };
+
+    void setGlobalPos(Vector2D& v);
+
+    Transform* getParent() { return parent_; };
     
     Transform& operator+(const Transform& t);
     Transform& operator-(const Transform& t);
