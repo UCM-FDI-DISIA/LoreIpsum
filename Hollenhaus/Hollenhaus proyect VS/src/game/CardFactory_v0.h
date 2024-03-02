58d3e558a6c43528f9c8b9acc54d0b9f7f5bc1c9
samuel <samuga05@ucm.es>
2024-03-02 20:08:58 +0100

    herencia card factory

--- Hollenhaus/Hollenhaus proyect VS/src/game/CardFactory_v0.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/CardFactory_v0.h
@@ -1,21 +1,20 @@
 #pragma once
 
 
-#include <vector>
-#include "../sdlutils/SDLUtils.h"
-
-class Vector2D;
+#include "CardFactory.h"
 
 
-
-class CardFactory_v0 {
+class CardFactory_v0 : public CardFactory{
 
 	float cardScale = 0.55f;
 	
 public:
-	ecs::entity_t createCard(Vector2D pos, int cost, int value, std::string& sprite, bool unblockable, std::vector<SDLUtils::CardEffect>& effects) ;
+	CardFactory_v0() : CardFactory() {};
+	~CardFactory_v0(){};
+
+	ecs::entity_t createCard(Vector2D pos, int cost, int value, std::string& sprite, bool unblockable, std::vector<SDLUtils::CardEffect>& effects)override ;
 	
-	void createHand() ;
+	void createHand() override;
 	
 	//ecs::entity_t createDropDetector(Vector2D pos = Vector2D(0,0)) ;
 	//ecs::entity_t createDropDetector_v2(Vector2D pos = Vector2D(0,0)) ;
