aea8b1aacc8dccbcc4deb2ee835e558504cab091
Lparres2000 <Lparres2000@gmail.com>
2024-03-03 18:17:33 +0100

    Comentarios y limpieza

--- Hollenhaus/Hollenhaus proyect VS/src/game/DragManager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/DragManager.cpp
@@ -91,7 +91,7 @@ void DragManager::OnLeftClickUp()
 			int y = dropDetector->getBoardPos().getY();
 			Card* card = dragTransform->getEntity()->getComponent<Card>();
 			
-			CellData::Owner playerTurn = GameStateMachine::instance()->getMngr()->getHandler(ecs::hdlr::MATCH_MANAGER)->getComponent<MatchManager>()->GetPlayerTurn();
+			CellData::Owner playerTurn = mngr_->getHandler(ecs::hdlr::MATCH_MANAGER)->getComponent<MatchManager>()->GetPlayerTurn();
 			boardManager->setCard(x, y, card, playerTurn);
 		}
 		else {//sino, devolvemos la carta a su posicion inicial
--- Hollenhaus/Hollenhaus proyect VS/src/game/SamuState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/SamuState.cpp
@@ -40,10 +40,10 @@ void SamuState::update()
 {
 	GameState::update();
 
-	board->getComponent<BoardManager>()->updateScore();
+	board->getComponent<BoardManager>()->updateScore();	// Esto puede ser un problema de performance
 
-	std::cout << "Player 1 points: " << board->getComponent<BoardManager>()->getPlayer1Points() << std::endl;
-	std::cout << "Player 2 points: " << board->getComponent<BoardManager>()->getPlayer2Points() << std::endl;
+	//std::cout << "Player 1 points: " << board->getComponent<BoardManager>()->getPlayer1Points() << std::endl;
+	//std::cout << "Player 2 points: " << board->getComponent<BoardManager>()->getPlayer2Points() << std::endl;
 
 }
 
@@ -55,39 +55,44 @@ void SamuState::render() const
 void SamuState::onEnter()
 {
 	TuVieja("\nEntering in SamuState");
+
 	// Entidad match manager para preguntar por los turnos. La entidad es un Handler para tener acesso a ella facilmente
 	auto matchManager = Instantiate();
 	GameStateMachine::instance()->getMngr()->setHandler(ecs::hdlr::MATCH_MANAGER, matchManager);
 	matchManager->addComponent<MatchManager>(4, MatchManager::TurnState::TurnJ1);
 
-	cardFact = new CardFactory_v0();
-
+	// Factoría del tablero. Generamos el tablero de juego.
 	boardFact = new BoardFactory(4, 4);
 	board = boardFact->createBoard();
 
+	// Factoría de cartas. Con ella generamos la mano inicial
+	cardFact = new CardFactory_v0();
 	cardFact->createHand();
 
+	// Drag Manager se encarga de gestionar el drag de todas las cartas
 	ecs::entity_t ent = Instantiate();
 	ent->addComponent<DragManager>();
 	ent->getComponent<DragManager>()->setBoardManager(board->getComponent<BoardManager>());
 
+	// Imágen de fondo
 	ecs::entity_t background = Instantiate();
 	background->addComponent<Transform>();
 	background->getComponent<Transform>()->getGlobalScale().set(0.42, 0.56);
 	background->addComponent<SpriteRenderer>("board");
 	background->setLayer(-1);
 
+	// Texto de prueba
 	ecs::entity_t pruebaTxt = Instantiate(Vector2D(400, 50));
 	pruebaTxt->addComponent<TextComponent>("Buenas tardes a los que ya han comido", "8bit_16pt", SDL_Color({ 255, 255, 255, 255 }), 350, TextComponent::BoxPivotPoint::CenterCenter, TextComponent::TextAlignment::Center);
 
-	// Creación del botón de J1 para acabar su turno
-	ecs::entity_t endTurnButtonJ1 = Instantiate(Vector2D(70, 500));
+	// Creación del botón de J1 para acabar su turno (debug por consola)
+	ecs::entity_t endTurnButtonJ1 = Instantiate(Vector2D(60, 500));
 	endTurnButtonJ1->addComponent<SpriteRenderer>("EndTurnButton");
 	endTurnButtonJ1->addComponent<BoxCollider>();
 	endTurnButtonJ1->addComponent<EndTurnButton>(MatchManager::TurnState::TurnJ1);
 
-	// Creación del botón de J2 para acabar su turno
-	ecs::entity_t endTurnButtonJ2 = Instantiate(Vector2D(70, 100));
+	// Creación del botón de J2 para acabar su turno (debug por consola)
+	ecs::entity_t endTurnButtonJ2 = Instantiate(Vector2D(60, 100));
 	endTurnButtonJ2->addComponent<SpriteRenderer>("EndTurnButton");
 	endTurnButtonJ2->addComponent<BoxCollider>();
 	endTurnButtonJ2->addComponent<EndTurnButton>(MatchManager::TurnState::TurnJ2);
