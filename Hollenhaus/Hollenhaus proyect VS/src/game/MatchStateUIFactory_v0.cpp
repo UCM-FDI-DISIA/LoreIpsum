85b73a711e3e495b5297943daba321d8419bdbea
Lparres2000 <Lparres2000@gmail.com>
2024-03-11 16:52:55 +0100

    Creación MatchStateFactory_v0

---
+++ Hollenhaus/Hollenhaus proyect VS/src/game/MatchStateUIFactory_v0.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "MatchStateUIFactory_v0.h"
+
+ecs::entity_t MatchStateUIFactory_v0::createVisual_NextTurnButton()
+{
+    return ecs::entity_t();
+}
+
+ecs::entity_t MatchStateUIFactory_v0::createVisual_ActionPointsCounter()
+{
+    return ecs::entity_t();
+}
+
+ecs::entity_t MatchStateUIFactory_v0::createVisual_ScoreCounter()
+{
+    return ecs::entity_t();
+}
+
+ecs::entity_t MatchStateUIFactory_v0::createVisual_PlayerTurnIndicator()
+{
+    return ecs::entity_t();
+}
