94f145702fd21a07496a15e1d363b409b57f2592
samuel <samuga05@ucm.es>
2024-03-26 14:27:36 +0100

    Algoritmos pasados a clases

--- Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.cpp
@@ -9,282 +9,9 @@
 #include <SDL.h>
 
 
-struct CartaColocada {
 
-	CartaColocada() {};
 
-	CartaColocada(int indice, Vector2D pos)
-		:indice(indice), pos(pos) {};
 
-	int indice;//indice en la mano
-	Vector2D pos;//posicon en el tablero (-1,-1) no se coloca
-};
-
-struct TuplaSolucion {
-
-	TuplaSolucion(int cartasRobadas, std::vector<CartaColocada> cartas)
-		: cartasRobadas(cartasRobadas), cartas(cartas) {};
-	TuplaSolucion() {};
-
-	int cartasRobadas;
-	std::vector<CartaColocada> cartas;//hand + cartas robadas	
-};
-
-struct State {
-
-	State() {
-
-		std::vector<Card*> cards{ nullptr,nullptr,nullptr };
-		std::vector<Players::Owner> owners{ Players::NONE,Players::NONE,Players::NONE };
-
-		for (int i = 0; i < 4; i++) {
-			_boardCards.push_back(cards);
-			_boardOwners.push_back(owners);
-		}
-
-	};
-
-	std::vector<std::vector<bool>> _boardBools;
-	std::vector<std::vector<Card*>> _boardCards;
-	std::vector<std::vector<Players::Owner>> _boardOwners;
-
-	std::vector<Card*> playerHand;
-	std::vector<Card*> enemyHand;
-
-	std::vector<Card*> playerDeck;
-	std::vector<Card*> enemyDeck;
-
-	int actionPoints;
-
-	TuplaSolucion _jugada;
-
-	//devuelve la diferencia de puntos entre el jugador y el enemigo 
-	//PuntosJugador -PuntosEnemigo
-	// el valor es positivo si el jugador tiene mas puntos, y negativo si tiene menos
-	int heuristic() const {
-		return 1;
-	};
-
-	void apply(TuplaSolucion jugada, bool isPlayer) {
-
-		//guardar la jugada que lleva a este estado
-		_jugada = jugada;
-
-		int nCartasColocadas = 0;
-
-		if (isPlayer) {
-
-			//robarCartas
-			for (int i = 0; i < jugada.cartasRobadas; i++) {
-
-				Card* c = playerDeck.back();
-				playerDeck.pop_back();
-
-				playerHand.push_back(c);
-			}
-
-
-			//poner cartas en el mazo
-			for (int i = 0; i < jugada.cartas.size(); i++) {
-
-				//si se coloca la carta
-				if (jugada.cartas[i].pos.getX() != -1) {
-
-					int posX = jugada.cartas[i].pos.getX();
-					int posY = jugada.cartas[i].pos.getY();
-
-					//poner la carta en el tablero
-					_boardBools[posX][posY] = true;
-					_boardOwners[posX][posY] = Players::PLAYER1;
-					_boardCards[posX][posY] = playerHand[(i - nCartasColocadas)];
-
-					//quitar la carta de la mano
-					playerHand.erase(playerHand.begin() + (i - nCartasColocadas));
-
-					nCartasColocadas++;
-				}
-
-			}
-
-		}
-		else {
-			//robarCartas
-			for (int i = 0; i < jugada.cartasRobadas; i++) {
-
-				Card* c = enemyDeck.back();
-				enemyDeck.pop_back();
-
-				enemyHand.push_back(c);
-			}
-
-			//poner cartas en el mazo
-			for (int i = 0; i < jugada.cartas.size(); i++) {
-
-				//si se coloca la carta
-				if (jugada.cartas[i].pos.getX() != -1) {
-
-					int posX = jugada.cartas[i].pos.getX();
-					int posY = jugada.cartas[i].pos.getY();
-
-					//poner la carta en el tablero
-					_boardBools[posX][posY] = true;
-					_boardOwners[posX][posY] = Players::PLAYER2;
-					_boardCards[posX][posY] = playerHand[(i - nCartasColocadas)];
-
-					//quitar la carta de la mano
-					enemyHand.erase(enemyHand.begin() + (i - nCartasColocadas));
-
-					nCartasColocadas++;
-
-				}
-
-			}
-
-
-		}
-
-	}
-};
-
-
-void posiblesTurnos(
-	int cartaActual, int nCartas,
-	std::vector<std::vector<CartaColocada>>& soluciones,
-	std::vector<CartaColocada>& solAct, 
-	int puntosRestantes,
-	const std::vector<Card*>& playerHand,//mano del jugador, ref const
-	std::vector<std::vector<bool>>& _board // true si ocupada, false si libre
-)
-{
-	if (cartaActual == nCartas ) {
-		soluciones.push_back(solAct);
-		return;
-	}
-
-	//asumiendo que los tableros son rectangulares
-	int sizeX = _board.size();
-	int sizeY = _board[0].size();
-
-	bool esValida = true;
-
-	//si la carta cuesta mas puntos de los que me quedan
-	if (playerHand[cartaActual]->getCost() > puntosRestantes) {
-		esValida = false;
-	}
-
-	if (esValida) {
-
-		for (int i = 0; i < sizeX; i++) {
-
-			for (int j = 0; j < sizeY; j++) {
-
-				//si casilla no ocupada
-				if (!_board[i][j]) {
-
-
-					//actualizar solAct
-					_board[i][j] = true;
-					solAct.push_back(CartaColocada(cartaActual,Vector2D(i,j)));
-
-					puntosRestantes -= playerHand[cartaActual]->getCost();
-
-					//siguiente nivel
-					posiblesTurnos(cartaActual + 1, nCartas, soluciones, solAct, puntosRestantes,
-						playerHand, _board);
-
-					//desactualizar solucion Actual	
-					solAct.pop_back();
-					_board[i][j] = false;
-
-					puntosRestantes += playerHand[cartaActual]->getCost();
-
-				}
-
-			}
-		}
-
-	}
-
-	//actualizar solucion actual con (-1,-1) No colocar	
-	solAct.push_back(CartaColocada(cartaActual, Vector2D(-1,-1)));
-
-	//siguiente nivel
-	posiblesTurnos(cartaActual + 1, nCartas, soluciones, solAct, puntosRestantes,
-		playerHand, _board);
-
-	//desactualizar solucion Actual	
-	solAct.pop_back();
-
-}
-
-std::vector<TuplaSolucion> calcularTurno(State s,bool isPlayer) {
-
-	std::vector<TuplaSolucion> solucionesGlobal;
-
-	int i = 0;
-
-	int aux = s.actionPoints;
-
-
-	//la cantidad de veces que se puede robar es el minimo entre los puntos de accion y el tamaño del mazo
-	if (isPlayer) {
-		aux = fmin(s.actionPoints, s.playerDeck.size());
-	}
-	else {
-		aux = fmin(s.actionPoints, s.enemyDeck.size());
-	}
-
-	while(i <= aux)
-	{
-		if (i > 0) {
-			if (isPlayer) {
-				//robar carta
-				Card* c = s.playerDeck.back();
-
-				s.playerDeck.pop_back();
-				s.playerHand.push_back(c);
-			}
-			else {
-				//robar carta
-				Card* c = s.enemyDeck.back();
-
-				s.enemyDeck.pop_back();
-				s.enemyHand.push_back(c);
-			}
-			s.actionPoints--;//- N coste de robar carta
-		}
-
-		//actualizar solucionActual
-		//act puntosRestantes
-
-		std::vector<std::vector<CartaColocada>> soluciones;
-		std::vector<CartaColocada> solAct;
-
-		if (isPlayer) {
-			posiblesTurnos(0, s.playerHand.size(), soluciones, solAct, s.actionPoints,
-				s.playerHand, s._boardBools );//hand + cartas robadas	
-		}
-		else {
-			posiblesTurnos(0, s.enemyHand.size(), soluciones, solAct, s.actionPoints,
-				s.enemyHand, s._boardBools);//hand + cartas robadas	
-		}
-
-		//actualizar la lista de soluciones global(TuplaSolucion)
-
-		int j = 0;
-		for (auto& s : soluciones) {
-			solucionesGlobal.push_back(TuplaSolucion{ i,s });
-		}
-
-		i++;
-	}
-
-	return solucionesGlobal;
-}
-
-std::vector<State> all_posible_next_states(const State& s, bool isPlayer);
-
-int minimax(int depth, int h, bool isPlayer, const State& current_state, State*& best);
 
 IA_manager::IA_manager()
 {
@@ -376,7 +103,143 @@ void IA_manager::evaluateState()
 
 
 
-std::vector<State> all_posible_next_states(const State& s,bool isPlayer) {
+void IA_manager::posiblesTurnos(
+	int cartaActual, int nCartas,
+	std::vector<std::vector<CartaColocada>>& soluciones,
+	std::vector<CartaColocada>& solAct,
+	int puntosRestantes,
+	const std::vector<Card*>& playerHand,//mano del jugador, ref const
+	std::vector<std::vector<bool>>& _board // true si ocupada, false si libre
+)
+{
+	if (cartaActual == nCartas) {
+		soluciones.push_back(solAct);
+		return;
+	}
+
+	//asumiendo que los tableros son rectangulares
+	int sizeX = _board.size();
+	int sizeY = _board[0].size();
+
+	bool esValida = true;
+
+	//si la carta cuesta mas puntos de los que me quedan
+	if (playerHand[cartaActual]->getCost() > puntosRestantes) {
+		esValida = false;
+	}
+
+	if (esValida) {
+
+		for (int i = 0; i < sizeX; i++) {
+
+			for (int j = 0; j < sizeY; j++) {
+
+				//si casilla no ocupada
+				if (!_board[i][j]) {
+
+
+					//actualizar solAct
+					_board[i][j] = true;
+					solAct.push_back(CartaColocada(cartaActual, Vector2D(i, j)));
+
+					puntosRestantes -= playerHand[cartaActual]->getCost();
+
+					//siguiente nivel
+					posiblesTurnos(cartaActual + 1, nCartas, soluciones, solAct, puntosRestantes,
+						playerHand, _board);
+
+					//desactualizar solucion Actual	
+					solAct.pop_back();
+					_board[i][j] = false;
+
+					puntosRestantes += playerHand[cartaActual]->getCost();
+
+				}
+
+			}
+		}
+
+	}
+
+	//actualizar solucion actual con (-1,-1) No colocar	
+	solAct.push_back(CartaColocada(cartaActual, Vector2D(-1, -1)));
+
+	//siguiente nivel
+	posiblesTurnos(cartaActual + 1, nCartas, soluciones, solAct, puntosRestantes,
+		playerHand, _board);
+
+	//desactualizar solucion Actual	
+	solAct.pop_back();
+
+}
+
+
+std::vector<IA_manager::TuplaSolucion>  IA_manager::calcularTurno(State s, bool isPlayer) {
+
+	std::vector<TuplaSolucion> solucionesGlobal;
+
+	int i = 0;
+
+	int aux = s.actionPoints;
+
+
+	//la cantidad de veces que se puede robar es el minimo entre los puntos de accion y el tamaño del mazo
+	if (isPlayer) {
+		aux = fmin(s.actionPoints, s.playerDeck.size());
+	}
+	else {
+		aux = fmin(s.actionPoints, s.enemyDeck.size());
+	}
+
+	while (i <= aux)
+	{
+		if (i > 0) {
+			if (isPlayer) {
+				//robar carta
+				Card* c = s.playerDeck.back();
+
+				s.playerDeck.pop_back();
+				s.playerHand.push_back(c);
+			}
+			else {
+				//robar carta
+				Card* c = s.enemyDeck.back();
+
+				s.enemyDeck.pop_back();
+				s.enemyHand.push_back(c);
+			}
+			s.actionPoints--;//- N coste de robar carta
+		}
+
+		//actualizar solucionActual
+		//act puntosRestantes
+
+		std::vector<std::vector<CartaColocada>> soluciones;
+		std::vector<CartaColocada> solAct;
+
+		if (isPlayer) {
+			posiblesTurnos(0, s.playerHand.size(), soluciones, solAct, s.actionPoints,
+				s.playerHand, s._boardBools);//hand + cartas robadas	
+		}
+		else {
+			posiblesTurnos(0, s.enemyHand.size(), soluciones, solAct, s.actionPoints,
+				s.enemyHand, s._boardBools);//hand + cartas robadas	
+		}
+
+		//actualizar la lista de soluciones global(TuplaSolucion)
+
+		int j = 0;
+		for (auto& s : soluciones) {
+			solucionesGlobal.push_back(TuplaSolucion{ i,s });
+		}
+
+		i++;
+	}
+
+	return solucionesGlobal;
+}
+
+std::vector<IA_manager::State> IA_manager::all_posible_next_states(const State& s,bool isPlayer) {
 	std::vector<State> allStates;
 
 	for (auto& jugada : calcularTurno(s, isPlayer)) {
@@ -389,37 +252,26 @@ std::vector<State> all_posible_next_states(const State& s,bool isPlayer) {
 	return allStates;
 }
 
-
-
-int minimax(int depth,int h, bool isPlayer, const State& current_state,State*& best) {
+int IA_manager::minimax(int depth,int h, bool isPlayer, const State& current_state, State*& best) {
 
 	//si alcanza la profundidad indicada, devuelve el valor de la heurisitca
-	if (depth == h) {
-		return current_state.heuristic();
-	}
-
+	if (depth == h) { return current_state.heuristic(); }
+	
 	int value = 0;
-	//State* best = nullptr;
 
 	for (State& s : all_posible_next_states(current_state, isPlayer)) {
+		
 		int current = minimax(depth + 1, h, !isPlayer, s,best);
 
-		if (isPlayer) {
-			if (current > value) {
+		if (isPlayer && current > value) { //si es jugador, maximiza el valor			
+			value = current;
+			best = new State(s);		
+		}
+		else if (current < value) {//si es la IA, lo minimiza
 				value = current;
 				best = new State(s);
-			}
-		}
-		else {
-			if (current < value) {
-				value = current;
-				best = new State(s);
-
-			}
-		}
+		}	
 	}
-
-
 
 	return 0;
 }
--- Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.h
@@ -2,13 +2,163 @@
 
 #include "../ComponentUpdate.h"
 
+#include "../../../utils/Vector2D.h"
+
 class Card;
+
+
 
 
 
 class IA_manager : public ComponentUpdate
 {
 public:
+
+	struct CartaColocada {
+
+		CartaColocada() {};
+
+		CartaColocada(int indice, Vector2D pos)
+			:indice(indice), pos(pos) {};
+
+		int indice;//indice en la mano
+		Vector2D pos;//posicon en el tablero (-1,-1) no se coloca
+	};
+
+	struct TuplaSolucion {
+
+		TuplaSolucion(int cartasRobadas, std::vector<CartaColocada> cartas)
+			: cartasRobadas(cartasRobadas), cartas(cartas) {};
+		TuplaSolucion() {};
+
+		int cartasRobadas;
+		std::vector<CartaColocada> cartas;//hand + cartas robadas	
+	};
+
+	struct State {
+
+		State() {
+
+			std::vector<Card*> cards{ nullptr,nullptr,nullptr };
+			std::vector<Players::Owner> owners{ Players::NONE,Players::NONE,Players::NONE };
+
+			for (int i = 0; i < 4; i++) {
+				_boardCards.push_back(cards);
+				_boardOwners.push_back(owners);
+			}
+
+		};
+
+		std::vector<std::vector<bool>> _boardBools;
+		std::vector<std::vector<Card*>> _boardCards;
+		std::vector<std::vector<Players::Owner>> _boardOwners;
+
+		std::vector<Card*> playerHand;
+		std::vector<Card*> enemyHand;
+
+		std::vector<Card*> playerDeck;
+		std::vector<Card*> enemyDeck;
+
+		int actionPoints;
+
+		TuplaSolucion _jugada;
+
+		//devuelve la diferencia de puntos entre el jugador y el enemigo 
+		//PuntosJugador -PuntosEnemigo
+		// el valor es positivo si el jugador tiene mas puntos, y negativo si tiene menos
+		int heuristic() const {
+			return 1;
+		};
+
+		void apply(TuplaSolucion jugada, bool isPlayer) {
+
+			//guardar la jugada que lleva a este estado
+			_jugada = jugada;
+
+			int nCartasColocadas = 0;
+
+			if (isPlayer) {
+
+				//robarCartas
+				for (int i = 0; i < jugada.cartasRobadas; i++) {
+
+					Card* c = playerDeck.back();
+					playerDeck.pop_back();
+
+					playerHand.push_back(c);
+				}
+
+
+				//poner cartas en el mazo
+				for (int i = 0; i < jugada.cartas.size(); i++) {
+
+					//si se coloca la carta
+					if (jugada.cartas[i].pos.getX() != -1) {
+
+						int posX = jugada.cartas[i].pos.getX();
+						int posY = jugada.cartas[i].pos.getY();
+
+						//poner la carta en el tablero
+						_boardBools[posX][posY] = true;
+						_boardOwners[posX][posY] = Players::PLAYER1;
+						_boardCards[posX][posY] = playerHand[(i - nCartasColocadas)];
+
+						//quitar la carta de la mano
+						playerHand.erase(playerHand.begin() + (i - nCartasColocadas));
+
+						nCartasColocadas++;
+					}
+
+				}
+
+			}
+			else {
+				//robarCartas
+				for (int i = 0; i < jugada.cartasRobadas; i++) {
+
+					Card* c = enemyDeck.back();
+					enemyDeck.pop_back();
+
+					enemyHand.push_back(c);
+				}
+
+				//poner cartas en el mazo
+				for (int i = 0; i < jugada.cartas.size(); i++) {
+
+					//si se coloca la carta
+					if (jugada.cartas[i].pos.getX() != -1) {
+
+						int posX = jugada.cartas[i].pos.getX();
+						int posY = jugada.cartas[i].pos.getY();
+
+						//poner la carta en el tablero
+						_boardBools[posX][posY] = true;
+						_boardOwners[posX][posY] = Players::PLAYER2;
+						_boardCards[posX][posY] = playerHand[(i - nCartasColocadas)];
+
+						//quitar la carta de la mano
+						enemyHand.erase(enemyHand.begin() + (i - nCartasColocadas));
+
+						nCartasColocadas++;
+
+					}
+
+				}
+
+
+			}
+
+		}
+	};
+
+
+
+
+
+
+
+
+
 	IA_manager();
 	~IA_manager();
 
@@ -17,6 +167,25 @@ public:
 	void update() override;
 
 	void evaluateState();
+
+
+
+	void posiblesTurnos(
+		int cartaActual, int nCartas,
+		std::vector<std::vector<CartaColocada>>& soluciones,
+		std::vector<CartaColocada>& solAct,
+		int puntosRestantes,
+		const std::vector<Card*>& playerHand,//mano del jugador, ref const
+		std::vector<std::vector<bool>>& _board // true si ocupada, false si libre
+	);
+
+
+	std::vector<TuplaSolucion> calcularTurno(State s, bool isPlayer);
+
+	std::vector<State> all_posible_next_states(const State& s, bool isPlayer);
+
+	int minimax(int depth, int h, bool isPlayer, const State& current_state, State*& best);
+
 private:
 
 
