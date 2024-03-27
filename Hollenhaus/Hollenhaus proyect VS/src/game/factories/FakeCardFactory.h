d75e909e6f486110575672a4791171e095314360
Andrés García Navarro <andrga37@ucm.es>
2024-03-27 10:25:03 +0100

    cacahuetes con miopia

--- Hollenhaus/Hollenhaus proyect VS/src/game/factories/FakeCardFactory.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/factories/FakeCardFactory.h
@@ -1,5 +1,23 @@
 #pragma once
+namespace JsonData
+{
+	struct CardEffect;
+}
+
 class FakeCardFactory
 {
+
+
+public:
+
+	FakeCardFactory() {};
+	virtual ~FakeCardFactory() {};
+
+	virtual ecs::entity_t
+		createCard(Vector2D pos, int cost, int value, std::string& sprite, bool unblockable, std::vector<JsonData::CardEffect>& effects) = 0;
+
+	virtual ecs::entity_t createHand() = 0;
+	virtual void createDeck() = 0;
+	virtual void createDeckJ2() = 0;
 };
 
