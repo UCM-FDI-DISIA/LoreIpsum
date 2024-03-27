ef5ce7f8256a01f1db3badf471bdf1676bc8f196
Paigro <paigro9@gmail.com>
2024-03-27 01:24:21 +0100

    Comentario varios para el futuro.

--- Hollenhaus/Hollenhaus proyect VS/src/game/components/ShopComponent.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/ShopComponent.h
@@ -7,20 +7,25 @@ class ShopComponent : public ComponentUpdate
 	ShopComponent();
 	~ShopComponent();
 
-	//------Inicializacion del componente:
+	//------Inicializacion del componente.
 	void initComponent() override;
 
-	//------Genera las 4 cartas que puede comprar el jugador:
+	//------Genera las 4 cartas que puede comprar el jugador.
 	void generateCards();
-	//------Comprueba si una carta ha sido comprada por el jugador, es decir, esta en el drawer (cajon):
+	//------Comprueba si una carta ha sido comprada por el jugador, es decir, esta en el drawer (cajon).
 	bool checkCardIsBought(int id);
+	//------Muestra las cartas disponibles para comprar.
+	void showCards();
+	//------Muestra los precios de las cartas.
+	void showPrizes();
 
 private:
 	//----Guarda las 4 cartas de la tienda.
 	int* shopCards; 
+
+	//------------------------------lo del precio de las cartas deberia de estar en el json. Todas en el mismo json.
+	//------------------------------array de vector2d de posiciones fijas para las cartas.
+	//------------------------------array de precios de las cartas. Sus posiciones son las de las cartas pero bajando la y.
 	
-
-
-
 };
 
