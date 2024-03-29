d47b5f94b2e281971e0a9c9d7c0ddd9a8fee7079
pokoli0 <psierr01@ucm.es>
2024-03-29 18:05:50 +0100

    samu ayuda

--- Hollenhaus/Hollenhaus proyect VS/src/game/components/NextText.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/NextText.cpp
@@ -44,9 +44,15 @@ void NextText::OnLeftClickUp()
 
 void NextText::reactToClick()
 {
-	dialogueReader_->NextNode();
-	//TuVieja("click en el textbox1.");
-	if (!click && boxCollider_->isCursorOver()) { //&& typeWriter_->ended()
+	if (typeWriter_->ended())
+	{
+		TuVieja("Cambio de nodo.");
+		dialogueReader_->NextNode();
+	}
+
+	//NO DETECTA EL COLLIIIDIIER AAA @SAMU AYUDA
+	if (!click && boxCollider_->isCursorOver() && typeWriter_->ended()) 
+	{
 		TuVieja("click en el textbox.");
 		dialogueReader_->NextNode();
 	}
