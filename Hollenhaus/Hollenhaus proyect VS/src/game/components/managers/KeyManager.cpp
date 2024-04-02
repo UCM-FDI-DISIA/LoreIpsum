b5c3527a0f83929eda3b081c4d763e481f65ef80
Jimbo <javicentear@gmail.com>
2024-04-02 16:54:07 +0200

    una cosa del keymanager

--- Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/KeyManager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/KeyManager.cpp
@@ -1,35 +1,38 @@
 #include "pch.h"
+
 #include "KeyManager.h"
 #include "Manager.h"
+
 #include "../basics/SpriteRenderer.h"
 #include "../basics/Transform.h"
+#include "../basics/TextComponent.h"
 
 #include <sstream>
 
-KeyManager::KeyManager() : tr_(), pos_(), lastKey_(0), addToY_(100) {}
+KeyManager::KeyManager() : addToY_(100), lastKey_(0), tr_(), pos_() {}
 
 void 
 KeyManager::initComponent() {
 	tr_ = ent_->getComponent<Transform>();
+	assert(tr_ != nullptr);
+
 	pos_ = tr_->getGlobalPos() + Vector2D(10, 0); // El numero inicial vector posicion de la key hay que cambiarlo segun el tamaño de la imagen del libro
-
-	imageIDs_.reserve(N);
-
-	for (int i = 0; i < N; ++i) {
-		std::stringstream p;
-		p << "picto" << i + 1;
-		imageIDs_.emplace_back(p.str());
-	}
 }
 
 void
 KeyManager::addKey(std::string s) {
-	ecs::entity_t e = Instantiate(pos_);
+	ecs::entity_t e = Instantiate(pos_); // Creamos la imagen
 
 	e->getComponent<Transform>()->addParent(getEntity()->getComponent<Transform>());
 	e->addComponent<SpriteRenderer>(s);
 
-	images_.emplace_back(e);
+	// Creamos el texto
+	ecs::entity_t txt = Instantiate(pos_ + Vector2D(e->getComponent<SpriteRenderer>()->getImageSize().getX(), 0));
+
+	txt->addComponent<Transform>()->addParent(e->getComponent<Transform>());
+	txt->addComponent<TextComponent>(descs_[lastKey_]);
+
+	keys_.emplace_back(e, txt);
 	pos_ = pos_ + Vector2D(0, addToY_);
 
 	++lastKey_;
