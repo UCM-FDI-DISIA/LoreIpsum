635bae720e4854e322bb25d6dad946693f3ea61a
samuel <samuga05@ucm.es>
2024-04-10 10:22:49 +0200

    multiplayer endGame

---
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MultiplayerEndGameState.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "MultiplayerEndGameState.h"
+
+MultiplayerEndGameState::MultiplayerEndGameState()
+{
+	TuVieja("Loading MultiplayerEndGameState");
+}
+
+MultiplayerEndGameState::~MultiplayerEndGameState()
+{
+}
+
+void MultiplayerEndGameState::refresh()
+{
+	GameState::refresh();
+}
+
+void MultiplayerEndGameState::update()
+{
+	GameState::update();
+}
+
+void MultiplayerEndGameState::render() const
+{
+	GameState::render();
+}
+
+void MultiplayerEndGameState::onEnter()
+{
+	TuVieja("ENTER MultiplayerEndGameState");
+
+}
+
+void MultiplayerEndGameState::onExit()
+{
+	TuVieja("EXIT MultiplayerEndGameState");
+
+}
---
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MultiplayerEndGameState.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameState.h"
+
+class MultiplayerEndGameState
+    :public GameState
+{
+public:
+    MultiplayerEndGameState();
+    virtual ~MultiplayerEndGameState();
+
+
+    void refresh() override;
+    void update() override;
+    void render() const override;
+
+
+    void onEnter() override;
+    void onExit() override;
+
+private:
+};
+
