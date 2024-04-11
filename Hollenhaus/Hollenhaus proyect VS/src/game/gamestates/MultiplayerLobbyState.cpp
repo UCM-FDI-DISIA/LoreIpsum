544ba144112f830a8c80f4c854376fba9592821e
Lparres2000 <Lparres2000@gmail.com>
2024-04-11 10:56:11 +0200

    Implementacion en escena lobby

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MultiplayerLobbyState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MultiplayerLobbyState.cpp
@@ -4,6 +4,9 @@
 #include "../components/InputForTextWritter.h"
 #include "../components/TextWritter.h"
 #include "../components/basics/TextComponent.h"
+#include "../components/CopyTextComponentOnClipboardButton.h"
+#include "../components/PasteOnTextComponentButton.h"
+
 
 MultiplayerLobbyState::MultiplayerLobbyState()
 {
@@ -53,7 +56,18 @@ void MultiplayerLobbyState::onEnter()
 	cuadroTexto->addComponent<TextWritter>();
 	cuadroTexto->addComponent<InputForTextWritter>();
 
+	ecs::entity_t pasteButton = Instantiate(Vector2D(sdlutils().width() / 2, sdlutils().height() / 2 + 100));
+	pasteButton->addComponent<SpriteRenderer>("black_box");
+	pasteButton->addComponent<BoxCollider>()->setAnchoredToSprite(true);
+	pasteButton->addComponent<PasteOnTextComponentButton>(cuadroTexto->getComponent<TextComponent>());
 
+	ecs::entity_t idHint = Instantiate(Vector2D(100, 100));
+	idHint->addComponent<TextComponent>("brr", "8bit_size_32", SDL_Color({ 0, 0,0 ,0 }), 300, Text::BoxPivotPoint::LeftTop, Text::TextAlignment::Left);
+
+	ecs::entity_t copyButton = Instantiate(Vector2D(400, 100));
+	copyButton->addComponent<SpriteRenderer>("black_box");
+	copyButton->addComponent<BoxCollider>()->setAnchoredToSprite(true);
+	copyButton->addComponent<CopyTextComponentOnClipboardButton>(idHint->getComponent<TextComponent>());
 }
 
 void MultiplayerLobbyState::onExit()
