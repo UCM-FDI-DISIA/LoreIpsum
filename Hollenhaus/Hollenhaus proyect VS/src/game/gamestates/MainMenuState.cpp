b2315228950985a61be9fa6c8ec62104d70bf49e
ariadnarc <ariadr02@ucm.es>
2024-04-21 10:04:03 +0200

    el main menu ahora tiene sentido

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MainMenuState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MainMenuState.cpp
@@ -53,11 +53,18 @@ void MainMenuState::onEnter()
 	//titleText->addComponent<TextComponent>("HÖLLENHAUS", "8bit_40pt", SDL_Color({ 255, 255, 255, 255 }), 450, TextComponent::BoxPivotPoint::CenterCenter, TextComponent::TextAlignment::Center);
 	//----Texto para un nuevo juego.
 	newGameButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 220));
-	newGameButton->addComponent<TextComponent>("NUEVA PARTIDA", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
+	newGameButton->addComponent<TextComponent>("MODO HISTORIA", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
 	newGameButton->addComponent<BoxCollider>();
 	newGameButton->getComponent<BoxCollider>()->setSize(Vector2D(300,40));
 	newGameButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
 	newGameButton->addComponent<NPC>(1, 0); // Esto es graciosisimo
+
+	multiplayerButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 170));
+	multiplayerButton->addComponent<TextComponent>("MULTIJUGADOR", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
+	multiplayerButton->addComponent<BoxCollider>();
+	multiplayerButton->getComponent<BoxCollider>()->setSize(Vector2D(300, 40));
+	multiplayerButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
+	multiplayerButton->addComponent<NPC>(13, 0);
 
 	//----Texto para continuar partida.
 	//ecs::entity_t continueText = Instantiate(Vector2D(400, 250));
@@ -66,27 +73,20 @@ void MainMenuState::onEnter()
 	//ecs::entity_t optionsText = Instantiate(Vector2D(400, 300));
 	//optionsText->addComponent<TextComponent>("OPTIONS", "8bit_24pt", SDL_Color({ 255, 255, 255, 255 }), 350, TextComponent::BoxPivotPoint::CenterCenter, TextComponent::TextAlignment::Center);
 	//----Texto para salir.
-	exitButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 125));
-	exitButton->addComponent<TextComponent>("SALIR", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
-	exitButton->addComponent<BoxCollider>();
-	exitButton->getComponent<BoxCollider>()->setSize(Vector2D(100,32));
-	exitButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(50, -16));
-	ih().insertFunction(InputHandler::MOUSE_LEFT_CLICK_DOWN, [this] { exitGame(); });
 
-	multiplayerButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 170));
-	multiplayerButton->addComponent<TextComponent>("MULTIJUGADOR", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
-	multiplayerButton->addComponent<BoxCollider>();
-	multiplayerButton->getComponent<BoxCollider>()->setSize(Vector2D(300, 40));
-	multiplayerButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
-	multiplayerButton->addComponent<NPC>(13, 0);
-	
-	optionsButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 80));
+	optionsButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 120));
 	optionsButton->addComponent<TextComponent>("OPCIONES", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
 	optionsButton->addComponent<BoxCollider>();
 	optionsButton->getComponent<BoxCollider>()->setSize(Vector2D(190, 40));
 	optionsButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-40, -20));
 	optionsButton->addComponent<NPC>(14, 0);
 
+	exitButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 70));
+	exitButton->addComponent<TextComponent>("SALIR", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
+	exitButton->addComponent<BoxCollider>();
+	exitButton->getComponent<BoxCollider>()->setSize(Vector2D(100,32));
+	exitButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(50, -16));
+	ih().insertFunction(InputHandler::MOUSE_LEFT_CLICK_DOWN, [this] { exitGame(); });
 
 
 	sdlutils().soundEffects().at("menutheme").play(-1);
