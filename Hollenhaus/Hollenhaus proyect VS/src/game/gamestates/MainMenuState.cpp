e1e391fb1245410ea5ca22b2865dc39d21b7d249
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-04-24 09:31:03 +0200

    Update MainMenuState.cpp

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MainMenuState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MainMenuState.cpp
@@ -61,12 +61,12 @@ void MainMenuState::onEnter()
 	newGameButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
 	newGameButton->addComponent<NPC>(GameStates::CITY, NPC::Type::BUTTON, "", true, true); // Esto es graciosisimo
 
-	multiplayerButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 170));
-	multiplayerButton->addComponent<TextComponent>("MULTIPLAYER", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
-	multiplayerButton->addComponent<BoxCollider>();
-	multiplayerButton->getComponent<BoxCollider>()->setSize(Vector2D(300, 40));
-	multiplayerButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
-	multiplayerButton->addComponent<NPC>(GameStates::MULTIPLAYER_LOBBY,0);
+	//multiplayerButton = Instantiate(Vector2D(sdlutils().width() - 200, sdlutils().height() - 170));
+	//multiplayerButton->addComponent<TextComponent>("MULTIPLAYER", "8bit_size_32", ROJO_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Right);
+	//multiplayerButton->addComponent<BoxCollider>();
+	//multiplayerButton->getComponent<BoxCollider>()->setSize(Vector2D(300, 40));
+	//multiplayerButton->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
+	//multiplayerButton->addComponent<NPC>(GameStates::MULTIPLAYER_LOBBY,0);
 
 
 	//----Texto para continuar partida.
