c916cbee0f0fbdebcae1ce319c17b3c20a59db93
samuel <samuga05@ucm.es>
2024-04-29 16:30:29 +0200

    arreglo bug IA + IA mas lista

--- Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/components/managers/IA_manager.cpp
@@ -284,7 +284,29 @@ std::vector<IA_manager::InfoJugada> IA_manager::calcularTurno(State s, bool isPl
 
 		//actualizar la lista de jugadasTotales(TuplaSolucion)
 		for (auto& s : partialPlays) {
-			allPosiblePlays.push_back(InfoJugada{ i,s });
+
+			if (i == 0) {
+
+
+
+				bool nula = true;
+				//buscar una jugada no nula
+
+				int j = 0;
+
+				while (j < s.size() && nula)
+				{
+					if (s[j].pos.getX() != 0 || s[j].pos.getY() != 0) {
+						nula = false;
+						allPosiblePlays.push_back(InfoJugada{ i,s });
+					}
+					j++;
+				}
+			}
+			else {
+				allPosiblePlays.push_back(InfoJugada{ i,s });
+
+			}
 		}
 	}
 
@@ -318,11 +340,11 @@ int IA_manager::minimax(int depth, int h, bool isPlayer, State& current_state, S
 
 		int current = minimax(depth + 1, h, !isPlayer, s, best);
 
-		if (isPlayer && current > bestValue) { //si es jugador, maximiza el valor			
+		if (isPlayer && current >= bestValue) { //si es jugador, maximiza el valor			
 			bestValue = current;
 			best = new State(s);
 		}
-		else if (!isPlayer && current < bestValue) {//si es la IA, lo minimiza
+		else if (!isPlayer && current <= bestValue) {//si es la IA, lo minimiza
 			bestValue = current;
 			best = new State(s);
 		}
