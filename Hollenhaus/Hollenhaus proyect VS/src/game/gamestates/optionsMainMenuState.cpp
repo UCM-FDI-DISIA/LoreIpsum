93364b303f056d110b0ba449596c6b3f3bf978bf
ariadnarc <ariadr02@ucm.es>
2024-04-30 13:22:27 +0200

    mas avances en opciones

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/optionsMainMenuState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/optionsMainMenuState.cpp
@@ -2,9 +2,13 @@
 
 #include "optionsMainMenuState.h"
 #include "../components/managers/Manager.h"
+#include "../components/basics/TextComponent.h"
+#include "../components/NPC.h"
 #include "../components/basics/SpriteRenderer.h"
 #include "../components/Button.h"
 #include "../components/basics/Transform.h"
+
+constexpr SDL_Color PEARL_HOLLENHAUS = { 226, 223, 210, 255 };
 
 OptionsMainMenuState::OptionsMainMenuState()
 {
@@ -48,6 +52,22 @@ void OptionsMainMenuState::onEnter()
 	returnButton->addComponent<NPC>(0, 0);
 	*/
 
+	audioText = Instantiate(Vector2D(sdlutils().width() - 600, sdlutils().height() - 425));
+	audioText->addComponent<TextComponent>("VOLUME", "8bit_size_32", PEARL_HOLLENHAUS, 200, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Left);
+	audioText->setLayer(1);
+
+	fullScreenText = Instantiate(Vector2D(sdlutils().width() - 400, sdlutils().height() - 300));
+	fullScreenText->addComponent<TextComponent>("PANTALLA COMPLETA", "8bit_size_32", PEARL_HOLLENHAUS, 400, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Center);
+	fullScreenText->addComponent<BoxCollider>();
+	fullScreenText->getComponent<BoxCollider>()->setSize(Vector2D(400, 40));
+	fullScreenText->getComponent<BoxCollider>()->setPosOffset(Vector2D(-200, -20));
+	
+	windowModeText = Instantiate(Vector2D(sdlutils().width() - 400, sdlutils().height() - 200));
+	windowModeText->addComponent<TextComponent>("MODO VENTANA", "8bit_size_32", PEARL_HOLLENHAUS, 300, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Center);
+	windowModeText->addComponent<BoxCollider>();
+	windowModeText->getComponent<BoxCollider>()->setSize(Vector2D(300, 40));
+	windowModeText->getComponent<BoxCollider>()->setPosOffset(Vector2D(-150, -20));
+
 	returnButton = Instantiate(Vector2D(10, 10));
 	returnButton->addComponent<Transform>();
 	returnButton->addComponent<BoxCollider>();
@@ -60,4 +80,5 @@ void OptionsMainMenuState::onEnter()
 
 void OptionsMainMenuState::onExit()
 {
+	GameStateMachine::instance()->getMngr()->Free();
 }
