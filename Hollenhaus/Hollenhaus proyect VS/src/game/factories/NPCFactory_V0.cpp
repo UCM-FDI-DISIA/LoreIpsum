5713a0ac7916bcdc5404370eb163f084c021ca4f
cynthia <118978961+cyntrist@users.noreply.github.com>
2024-05-07 06:50:06 +0200

    por la puta cara

--- Hollenhaus/Hollenhaus proyect VS/src/game/factories/NPCFactory_V0.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/factories/NPCFactory_V0.cpp
@@ -8,17 +8,22 @@
 ecs::entity_t NPCFactory_V0::createNPC(JsonData::NPCData info, ecs::entity_t parent)
 {
 	ecs::entity_t npc = Instantiate();
-	npc->addComponent<Transform>();
+
 	npc->addComponent<SpriteRenderer>(info.getSprite());
 	if (info.getSprite() == "whiteRect") { npc->getComponent<SpriteRenderer>()->setOpacity(0); } // whiteRect haria de boton
+
 	npc->addComponent<BoxCollider>();
+
 	npc->getComponent<Transform>()->addParent(parent->getComponent<Transform>());
-	npc->getComponent<Transform>()->getRelativeScale().set(info.getScale().getX(), info.getScale().getY());
+	npc->getComponent<Transform>()->setRelativeScale(info.getScale().getX(), info.getScale().getY());
 	Vector2D pos{ GameStateMachine::instance()->getCurrentState()->getLastPaulPos().getX() + info.getPos().getX(), info.getPos().getY()};
 	npc->getComponent<Transform>()->setGlobalPos(pos);
 	npc->getComponent<BoxCollider>()->setAnchoredToSprite(true);
+
+
 	npc->addComponent<NPC>(info.getScene(), info.getType(), info.getName(), true, true);
 	npc->setLayer(info.getLayer());
+
 	if (info.getSprite() != "whiteRect"){
 		npc->addComponent<ShineComponent>();
 		npc->getComponent<ShineComponent>()->addEnt(npc->getComponent<SpriteRenderer>(), info.shine());
