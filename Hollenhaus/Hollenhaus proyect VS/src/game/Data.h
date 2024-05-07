3ee1b774a8fd6fc54fb9859479ef2c1deb0092ec
cynthia <118978961+cyntrist@users.noreply.github.com>
2024-05-07 05:58:30 +0200

    Revert "DINAMITA"
    This reverts commit 978a30178dac0d76063a353bcd3f9c3a4912c57b.

--- Hollenhaus/Hollenhaus proyect VS/src/game/Data.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/Data.h
@@ -13,8 +13,6 @@ MAX_CARDS_MAZE = 20, // Maximo de cartas en el mazo
 // Cantidad de cartas de la tienda. Cambiar en shopComponent tambien.
 CARDS_IN_SHOP = 4;
 
-class RandomNumberGenerator;
-
 class Data
 {
 private:
@@ -45,8 +43,6 @@ private:
 
 	std::list<int> thisCaseClues; // Id de las cartas conseguidas durante el caso (deben ser aniadidas al drawer y aqui al ser conseguidass)
 
-	RandomNumberGenerator& rand_; // Para generar numeros aleatorios.
-
 	enum WINNER {
 		NONE,
 		TIE,
@@ -58,6 +54,7 @@ private:
 	TCPsocket rival;
 
 	bool isHost = false;
+
 public:
 
 	//------Constructora y destructora:
@@ -165,7 +162,6 @@ public:
 	void EmptyNPCS();
 	//----Vaciado del array de cartas de la tienda. Lo pone todo a (-1 ,-1, -1, -1).
 	void EmptyShopCards();
-	//----Vaciado del array del mazo con las posiciones de cada uno.
 	void EmptyMaze_With_pos();
 
 
@@ -179,5 +175,5 @@ public:
 	void setIsHost(bool b);
 	bool getIsHost();
 
-	void reset();
+
 };
