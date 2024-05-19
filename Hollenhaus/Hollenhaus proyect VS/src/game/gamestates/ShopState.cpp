d736278e1e955de5444f7a3dcf38d63c904d0a9a
Paigro <paigro9@gmail.com>
2024-05-19 14:57:16 +0200

    Tienda medio arreglada.

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/ShopState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/ShopState.cpp
@@ -209,13 +209,13 @@ void ShopState::cardSelected(int prize)
 
 void ShopState::deSelected()
 {
-	if (mngr().getEntities(ecs::grp::COINS).capacity() == 0)
+	if (mngr().getEntities(ecs::grp::COINS).capacity() != 0)
 	{
-	for (int i = 0; i < 8; i++)
-	{
-		mngr().getEntities(ecs::grp::COINS)[i]->getComponent<SpriteRenderer>()->setTexture("moneda_tienda");
-	}
-	updateCoins();
+		for (int i = 0; i < mngr().getEntities(ecs::grp::COINS).size(); i++)
+		{
+			mngr().getEntities(ecs::grp::COINS)[i]->getComponent<SpriteRenderer>()->setTexture("moneda_tienda");
+		}
+		updateCoins();
 	}
 }
 
