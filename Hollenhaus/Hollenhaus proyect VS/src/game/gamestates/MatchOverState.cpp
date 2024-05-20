057f2b1f3118521856f9bd6279107fb0596248a7
Paigro <paigro9@gmail.com>
2024-05-20 02:36:20 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    Mañana arreglo lo que se me ha olvidado como solucionar.

--- Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MatchOverState.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/MatchOverState.cpp
@@ -8,8 +8,7 @@
 #include "../components/NPC.h"
 #include "./game/components/Button.h"
 #include "./game/components/Clickable.h"
-//#include "../../sdlutils/SDLUtils.h"
-
+#include "../components/ClickableText.h"
 // Factorias:
 #include "../factories/Factory.h"
 #include "../factories/FakeCardFactory_v0.h"
@@ -45,27 +44,20 @@ void MatchOverState::onEnter()
 	factory = new Factory();
 	factory->SetFactories(static_cast<FakeCardFactory*>(new FakeCardFactory_v0()));
 
-	//------Boton para volver:
-	ecs::entity_t exit = Instantiate();
-	exit->addComponent<Transform>();
-	exit->addComponent<SpriteRenderer>("boton_flecha");
-	exit->addComponent<BoxCollider>();
-	Vector2D exitPos(10, 10);
-	exit->getComponent<Transform>()->setGlobalPos(exitPos);
-	exit->getComponent<BoxCollider>()->setAnchoredToSprite(true);
-	if (j2_ == "8") {
-		exit->addComponent<Button>();
-		exit->getComponent<Button>()->connectToButton([this]
-			{
-				GameStateMachine::instance()->setState(GameStates::ENDGAME);
-			});
-	}
-	else
+	auto continuar = Instantiate(Vector2D(sdlutils().width() - 400, sdlutils().height() - 150));
+	continuar->addComponent<TextComponent>("CONTINUAR", "space_grotesk_bold_32", Colors::PEARL_HOLLENHAUS, 200, Text::BoxPivotPoint::CenterCenter, Text::TextAlignment::Center);
+	continuar->addComponent<BoxCollider>();
+	continuar->getComponent<BoxCollider>()->setSize(Vector2D(220, 40));
+	continuar->getComponent<BoxCollider>()->setPosOffset(Vector2D(-100, -20));
+	if (j2_ == "8")
 	{
-		exit->addComponent<NPC>(1); // Lleva a la ciudad (1).
+		continuar->addComponent<NPC>(GameStates::ENDGAME, 0); // Esto es graciosisimo
 	}
-	exit->setLayer(1);
-	exit->addComponent<Clickable>("boton_flecha", true);
+	else 
+	{
+		continuar->addComponent<NPC>(GameStates::CITY, 0); // Esto es graciosisimo
+	}
+	continuar->addComponent<ClickableText>(Colors::PEARL_HOLLENHAUS, Colors::PEARL_CLICK, Colors::ROJO_HOLLENHAUS);
 
 	setWindow(data->getWinner());
 }
@@ -79,21 +71,8 @@ void MatchOverState::onExit()
 
 void MatchOverState::setWindow(int lastWinner)
 {
-	//j2_ = "12";
-	j2_ = GameStateMachine::instance()->getCurrentState()->getLastDefeatedNPC(); // PAIGRO AQUI descomentar.
-	int id = sdlutils().npcs().at(j2_).cardToDrop();
-
-	if (j2_ == "6" || j2_ == "7" || j2_ == "8") // Ids de la tarotista.
-	{
-		_text = "caso:";
-		tarotista = true;
-	}
-	else // NPC normal.
-	{
-		_text = "batalla:";
-		tarotista = false;
-	}
-
+	_text = "batalla";
+	int id;
 
 	// Cual es el ultimo ganador de la partida?
 	switch (lastWinner)
@@ -104,8 +83,13 @@ void MatchOverState::setWindow(int lastWinner)
 		_souls = "0";
 		break;
 	case 2: // Jugador1:
-		if (tarotista) // Id de la tarotista.
+		j2_ = std::to_string(GameStateMachine::instance()->getCurrentState()->getLastDefeatedNPC());
+		id = sdlutils().npcs().at(j2_).cardToDrop();
+
+		if (j2_ == "6" || j2_ == "7" || j2_ == "8") // Ids de la tarotista.
 		{
+			_text = "caso";
+			tarotista = true;
 			data->AddMoney(TAROT_MONEY_GAIN);
 			_money = std::to_string(TAROT_MONEY_GAIN);
 			_souls = "1";
@@ -113,6 +97,8 @@ void MatchOverState::setWindow(int lastWinner)
 		}
 		else // NPC normal.
 		{
+			_text = "batalla";
+			tarotista = false;
 			data->AddMoney(NPC_MONEY_GAIN);
 			_money = std::to_string(NPC_MONEY_GAIN);
 			_souls = "0";
@@ -120,7 +106,7 @@ void MatchOverState::setWindow(int lastWinner)
 
 		if (id != -1)
 		{
-			auto card = createCard(id, Vector2D(570, 230));
+			auto card = createCard(id, Vector2D(500, 230));
 		}
 		break;
 	case 3: // Caso si gana el NPC o tatotista:
@@ -134,26 +120,33 @@ void MatchOverState::setWindow(int lastWinner)
 	}
 
 
+	//------Carta del fondo.
+	ecs::entity_t noCard = Instantiate(Vector2D());
+	noCard->getComponent<Transform>()->setGlobalPos(500, 230);
+	noCard->addComponent<SpriteRenderer>("no_card");
+	noCard->setLayer(0);
 	//------Texto fin batalla/caso:
 	ecs::entity_t matchOverText = Instantiate(Vector2D());
-	matchOverText->getComponent<Transform>()->setGlobalPos(400, 100);
-	matchOverText->addComponent<TextComponent>("Resultado fin de " + _text, Fonts::GROTESK_40, Colors::PEARL_HOLLENHAUS, 500, Text::CenterCenter, Text::Center);
+	matchOverText->getComponent<Transform>()->setGlobalPos(250, 200);
+	matchOverText->addComponent<TextComponent>("Resultado fin de " + _text, Fonts::GROTESK_24, Colors::ROJO_HOLLENHAUS, 500, Text::CenterCenter, Text::Left);
 	matchOverText->setLayer(1);
 	//------Texto fin batalla/caso:
 	ecs::entity_t cardWonText = Instantiate(Vector2D());
-	cardWonText->getComponent<Transform>()->setGlobalPos(600, 200);
-	cardWonText->addComponent<TextComponent>("Carta ganada:", Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::CenterCenter, Text::Center);
+	cardWonText->getComponent<Transform>()->setGlobalPos(550, 200);
+	cardWonText->addComponent<TextComponent>("Carta ganada", Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::CenterCenter, Text::Left);
 	cardWonText->setLayer(1);
 	//------Texto dinero ganado:
 	ecs::entity_t moneyWon = Instantiate(Vector2D());
-	moneyWon->getComponent<Transform>()->setGlobalPos(200, 200);
-	moneyWon->addComponent<TextComponent>("Dinero ganado:\n" + _money + " DM", Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::CenterCenter, Text::Center);
+	moneyWon->getComponent<Transform>()->setGlobalPos(150, 250);
+	moneyWon->addComponent<TextComponent>("Dinero ganado\n+" + _money + " DM", Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::LeftCenter, Text::Left);
 	moneyWon->setLayer(1);
 	//------Texto almas ganado:
 	ecs::entity_t soulsWon = Instantiate(Vector2D());
-	soulsWon->getComponent<Transform>()->setGlobalPos(200, 300);
-	soulsWon->addComponent<TextComponent>("Almas ganadas:\n" + _souls, Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::CenterCenter, Text::Center);
+	soulsWon->getComponent<Transform>()->setGlobalPos(150, 320);
+	soulsWon->addComponent<TextComponent>("Almas ganadas\n+" + _souls, Fonts::GROTESK_24, Colors::PEARL_HOLLENHAUS, 500, Text::LeftCenter, Text::Left);
 	soulsWon->setLayer(1);
+
+
 
 	// ---- Resetea el ganador a nulo al salir del estado ----
 	data->setWinner(0);
