8b10ba8bb8f3e81ec0fc940695ba9edea98e802d
cynthia <118978961+cyntrist@users.noreply.github.com>
2024-05-20 22:06:52 +0200

    firstState

---
+++ Hollenhaus/Hollenhaus proyect VS/src/game/gamestates/FirstState.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+//Checkml
+#include <game/checkML.h>
+#include "GameState.h"
+class FirstState :
+    public GameState
+{
+public:
+	FirstState();
+	~FirstState() override;
+	void update() override;
+	void render() const override;
+	void refresh() override;
+
+	void onEnter() override;
+	void onExit() override;
+};
+
