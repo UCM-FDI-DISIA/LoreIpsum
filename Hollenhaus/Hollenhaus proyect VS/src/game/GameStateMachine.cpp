bb009ed7c36be0e895f34c0e3721ec9751d56674
Jimbo <javicentear@gmail.com>
2024-05-21 19:47:42 +0200

    Update GameStateMachine.cpp

--- Hollenhaus/Hollenhaus proyect VS/src/game/GameStateMachine.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/GameStateMachine.cpp
@@ -52,6 +52,7 @@ constexpr Uint8 FADE_SPEED = 30;
 
 void GameStateMachine::init()
 {
+	currentState->loadDataIfExists();
 	//Estado incial
 	pushState(currentState);
 
@@ -117,7 +118,6 @@ GameStateMachine::GameStateMachine()
 
 	// settea la data en el current state para acceder a ella desde cualquier estado
 	currentState->setData(new Data());
-	currentState->loadDataIfExists();
 }
 
 // destructor
