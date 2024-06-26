603896f26c56ba550c157d5c94838ba7c04e468a
ariadnarc <ariadr02@ucm.es>
2024-06-26 11:38:41 +0200

    icono pequeño y ajustes

--- Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.cpp
+++ Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.cpp
@@ -19,7 +19,7 @@ ecs::entity_t SolCardFactory::CreateCard()
 	//añadir componentes
 	newCard->addComponent<SpriteRenderer>("solitaireCard");
 	newCard->addComponent<BoxCollider>();
-	//newCard->addComponent<SpriteRenderer>("solitaireCard");
+	//falta el componente de SolCardComponent
 
 
 	//set layer && size
@@ -27,16 +27,20 @@ ecs::entity_t SolCardFactory::CreateCard()
 	newCard->getComponent<Transform>()->setGlobalScale(cardScale.getX(),cardScale.getY());
 
 
-
-	//crear los hijos de la carta
-	auto iconBig = Instantiate(iconBigOffset);
+	//hijos de la carta
+	auto iconBig = Instantiate(iconBigOffset); //icono grande
 
 	iconBig->addComponent<SpriteRenderer>("spades");
 	iconBig->getComponent<Transform>()->setGlobalScale(iconBigScale.getX(), iconBigScale.getY());
-
 	iconBig->getComponent<Transform>()->addParent(newCard->getComponent<Transform>());
 	iconBig->setLayer(iconsLayer);
+	
+	auto iconSmall = Instantiate(iconSmallOffset); //icono pequenio
 
+	iconSmall->addComponent<SpriteRenderer>("spades");
+	iconSmall->getComponent<Transform>()->setGlobalScale(iconSmallScale.getX(), iconSmallScale.getY());
+	iconSmall->getComponent<Transform>()->addParent(newCard->getComponent<Transform>());
+	iconSmall->setLayer(iconsLayer);
 
 	return newCard;
 }
--- Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.h
@@ -15,15 +15,15 @@ public:
 
 private:
 
-	Vector2D cardScale = Vector2D(1,1);
-	Vector2D iconBigScale = Vector2D(1,1);
-	Vector2D iconSmallScale = Vector2D(1,1);
+	Vector2D cardScale = Vector2D(1, 1);
+	Vector2D iconBigScale = Vector2D(0.9, 0.9);
+	Vector2D iconSmallScale = Vector2D(0.3, 0.3);
 
 	int cardLayer = 1;
-	int iconsLayer = 2;//debe ser mayor que cardLayer
+	int iconsLayer = 2; //debe ser mayor que cardLayer
 
-	Vector2D iconBigOffset = Vector2D(-10, 20);
-	Vector2D iconSmallOffset = Vector2D(3, 35);
+	Vector2D iconBigOffset = Vector2D(-2, 29);
+	Vector2D iconSmallOffset = Vector2D(50, 2);
 
 
 };
