f6ed8cbfcd4496a5f3a0423d005f85830a7f090a
ariadnarc <ariadr02@ucm.es>
2024-06-27 19:43:30 +0200

    reajuste de tamaños para que quepa todo

--- Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.h
+++ Hollenhaus/Hollenhaus proyect VS/src/game/factories/SolCardFactory.h
@@ -15,20 +15,18 @@ public:
 
 private:
 
-	Vector2D cardScale = Vector2D(1, 1);
-	Vector2D iconBigScale = Vector2D(0.9, 0.9);
-	Vector2D iconSmallScale = Vector2D(0.3, 0.3);
-	Vector2D reverseScale = Vector2D(1, 1);
+	Vector2D cardScale = Vector2D(0.7, 0.7);
+	Vector2D iconBigScale = Vector2D(0.7, 0.7);
+	Vector2D iconSmallScale = Vector2D(0.35, 0.35);
+	Vector2D reverseScale = Vector2D(0.7, 0.7);
 
 	int cardLayer = 1;
 	int iconsLayer = 2; //debe ser mayor que cardLayer
 	int reverseLayer = 3; //debe ser mayor que el resto
-	Vector2D iconBigOffset = Vector2D(-2, 29);
-	Vector2D iconSmallOffset = Vector2D(50, 2);
-	Vector2D numberOffset = Vector2D(20, 15);
+	Vector2D iconBigOffset = Vector2D(10, 31);
+	Vector2D iconSmallOffset = Vector2D(30, 2);
+	Vector2D numberOffset = Vector2D(15, 15);
 
 	SDL_Color col; //color del numero de la carta
 	std::string textoCarta; //numero o letra que va a ir en la carta
-
-
 };
